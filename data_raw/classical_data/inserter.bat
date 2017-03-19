b26e6ba84812ca755b3d2af07b3ca655d50eaebd
Brightgalrs <ChaosRobie@gmail.com>
2017-03-19 01:38:11 -0400

    .gitignore update, removing .kar's and .mid's

--- data_raw/classical_data/inserter.bat
+++
@@ -1,3 +0,0 @@
-rem for %%i in (*.mid) do c:/Programmi/ActivePerl/bin/perl.exe inserter.pl comment.txt %%i
-
-for /F "tokens=*" %%i in ('dir /B /S *.mid') do c:/Programmi/ActivePerl/bin/perl.exe inserter.pl comment.txt %%i
